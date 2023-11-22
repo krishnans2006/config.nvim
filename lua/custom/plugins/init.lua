7c7e51ec2b37566dc4a9625d735fa8d542c4b0d6
Krishnan Shankar <krishnans2006@gmail.com>
2023-11-21 21:18:45 -0500

    Add custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,36 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Manage project files
+  'ahmedkhalf/project.nvim',
+
+  -- Dashboard
+  {
+    'goolord/alpha-nvim',
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+  },
+
+  -- Grammarly
+  'emacs-grammarly/lsp-grammarly',
+
+  -- Highlight usages
+  'RRethy/vim-illuminate',
+
+  -- Switch between undo branches
+  'mbbill/undotree',
+
+  -- WakaTime
+  'wakatime/vim-wakatime',
+
+  -- Minimalist/focus/narrow modes
+  'pocco81/true-zen.nvim',
+
+  -- SudaRead/SudaWrite to open or save with sudo
+  'lambdalisue/suda.vim',
+
+  -- GitHub Copilot
+  'github/copilot.vim',
+}
